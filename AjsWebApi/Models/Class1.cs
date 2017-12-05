b5430199a3cdcb64b0add959e22652130987152b
anil.hhc9@gmail.com <Hetero@9>
2017-12-05 10:06:39 +0530

    10am

---
+++ AjsWebApi/Models/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+
+namespace AjsWebApi.Models
+{
+    public class Class1
+    {
+        MongoClient mg;
+        MongoServerAddress msa;
+        MongoServer ms;
+    }
+}
