95104d8b37670ffca4a8694155ab4c12e2945a95
anil.hhc9@gmail.com <Hetero@9>
2017-12-07 09:34:24 +0530

    9:30am

---
+++ ConsoleTst/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleTst;
+namespace ConsoleTst
+{
+    class Program:Class1
+    {
+        static void Main(string[] args)
+        {
+            object obj;
+            obj = 100;
+            dynamic d = 20;
+            
+            string str = "something";
+            Console.WriteLine(str);
+            Console.WriteLine("obj value {0}", obj);
+            Console.ReadLine();
+        }
+    }
+}
