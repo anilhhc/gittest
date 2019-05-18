31c6520f92ed68261bb38063ea243243645f6954
ani <ani@ANIL>
2019-05-18 06:33:45 +0530

    7am

---
+++ HhcTst/Controllers/ExcelPivotController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Office.Interop.Excel;
+namespace HhcTst.Controllers
+{
+    public class ExcelPivotController : Controller
+    {
+        // GET: ExcelPivot
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Upload() { return View(); }
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+
+            return View();
+        }
+
+
+    }
+}
