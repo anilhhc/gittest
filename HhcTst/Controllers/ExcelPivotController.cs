7019b0bf38677d528f5b9986bb64343f520befad
ani <ani@ANIL>
2019-05-23 08:01:08 +0530

    8am

--- HhcTst/Controllers/ExcelPivotController.cs
+++ HhcTst/Controllers/ExcelPivotController.cs
@@ -20,7 +20,37 @@ namespace HhcTst.Controllers
 
             return View();
         }
+        private void GeData()
+        {
+            string connection = @"OLEDB;Provider=SQLOLEDB.1;Integrated Security=SSPI;Server=192.168.0.1\SQL2005;DataBase=Test;UID=sa;PWD=pass@123";
+            string command = "SELECT Column1,Column2,Column3,Column4,cast((Column5*1.00)/Column4 AS DECIMAL(16,2)) as Column5  FROM  PivotData";
+            Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            Excel.Workbook workbook = (Microsoft.Office.Interop.Excel.Workbook)app.Workbooks.Add(Type.Missing);
+            Excel.Worksheet sheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.ActiveSheet;
+            Excel.PivotCache pivotCache = app.ActiveWorkbook.PivotCaches().Add(Excel.XlPivotTableSourceType.xlExternal, (Excel.Range)sheet.get_Range("A1", "E10"));
+            pivotCache.Connection = connection;
+            pivotCache.MaintainConnection = true;
+            pivotCache.CommandText = command;
+            pivotCache.CommandType = Excel.XlCmdType.xlCmdSql;
+            Excel.PivotTables pivotTables = (Excel.PivotTables)sheet.PivotTables(Type.Missing);
+            Excel.PivotTable pivotTable = pivotTables.Add(pivotCache, app.ActiveCell, "PivotTable1", Type.Missing, Type.Missing);
+            pivotTable.SmallGrid = false;
+            pivotTable.ShowTableStyleRowStripes = true;
+            pivotTable.TableStyle2 = "PivotStyleLight1";
+            Excel.PivotFields rowField = (Excel.PivotFields)pivotTable.PivotFields(Type.Missing);
+            int fieldCount = rowField.Count;
 
+            for (int i = 1; i <= fieldCount; i++)
+            {
+                if ("Colunm" + i != "Colunm2" && "Colunm" + i != "Colunm5")
+                {
+                    Excel.PivotField field = (Excel.PivotField)pivotTable.PivotFields("Column" + i);
+                    field.Orientation = Excel.XlPivotFieldOrientation.xlRowField;
+                }
+            }
+
+            pivotTable.AddDataField(pivotTable.PivotFields("Column4"), "Sum of Column4", Excel.XlConsolidationFunction.xlSum);
+        }
 
     }
 }
